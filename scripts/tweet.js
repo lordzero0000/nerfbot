81ad3c7770e24af555283578ab6e91dcb125ee6f
Dio Brando <lordzero0000@gmail.com>
2016-03-22 19:13:19 -0700

    Added tweet script

---
+++ scripts/tweet.js
@@ -0,0 +1,42 @@
+// Description:
+//   This bot will tweet shit for you.
+// Commands:
+//   tweet it: this will tweet a random quote...
+//
+// Notes:
+// This may work... but everyone in the chat will tweet shit for you account... be careful...
+//
+//   These are from the scripting documentation: https://github.com/github/hubot/blob/master/docs/scripting.md
+
+var Twitter = require('twitter'),
+    client = new Twitter({
+      consumer_key: process.env.TWITTER_CONSUMER_KEY,
+      consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+      access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+      access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    }),
+    postTweet = (tweet) => {
+      var promise = new Promise((resolve, reject) => {
+        client.post('statuses/update', {status: tweet}, (error, tweet, response) => {
+          if(error) { return reject(error); };
+          console.log(response.body);
+          return resolve(response.body);
+        });
+      });
+      return promise;
+    },
+    getTweetUrl = (tweetInfo) => {
+      return "https://twitter.com/" + tweetInfo.user.screen_name + "/status/" + tweetInfo.id;
+    };
+
+module.exports = (robot) => {
+  robot.respond(/tweet this (.*)/i, (res) => {
+    var tweet = res.match[1];
+    postTweet(tweet)
+    .then((tweetRes) => {
+      return res.send("Tweeted!");
+    }).catch((err) => {
+      return res.send(err);
+    })
+  });
+};
